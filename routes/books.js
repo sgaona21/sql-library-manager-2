3e727e1de68ce6b55eb224a1c087129d3c0d2b7f
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-07-15 20:59:26 -0700

    CREATE new book route complete

--- routes/books.js
+++ routes/books.js
@@ -13,4 +13,19 @@ router.get('/', async (req, res) => {
     }
 });
 
+//renders the new book page
+router.get('/new', (req, res) => {
+    res.render('new-book', { book: {} })
+})
+
+//creates new book
+router.post('/new', async (req, res) => {
+    try {
+        const book = await Book.create(req.body);
+        res.redirect('/books')
+    } catch(error) {
+        res.status(500).send(error)
+    }
+})
+
 module.exports = router;
